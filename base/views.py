aa96bea16c2f51c685a3104719f6b58d2ebaa880
Dorota Bryczkowska <dorotabryczkowska755@gmail.com>
2025-05-06 21:23:19 +0200

    Updating tasks
    Support for editing the content of a user-selected task has been created

--- base/views.py
+++ base/views.py
@@ -2,7 +2,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.urls import reverse_lazy
 from .models import Task
 
@@ -18,6 +18,12 @@ class TaskDetail(DetailView):
 
 class TaskCreate(CreateView):
     "Django view for creating a new task."
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class TaskUpdate(UpdateView):
+    "Django view for updating an existing task."
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('tasks')
