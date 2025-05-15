c08440fb61df305d83f0c241d213942caefab9e8
Dorota Bryczkowska <dorotabryczkowska755@gmail.com>
2025-05-16 00:43:17 +0200

    Login View
    Created custom login view, added it to urls and created login/logout buttons on the front page.

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,9 @@
 '# Django URL configuration for the base app'
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView
 
 urlpatterns = [
+    path('login/', CustomLoginView.as_view(), name='login'),
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
     path('task-create/', TaskCreate.as_view(), name='task-create'),
--- base/views.py
+++ base/views.py
@@ -4,7 +4,19 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+
+from django.contrib.auth.views import LoginView, LogoutView
+
 from .models import Task
+
+class CustomLoginView(LoginView):
+    "Django view for custom login page."
+    template_name = 'base/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
 
 class TaskList(ListView):
     "Django view for displaying a list of tasks."
