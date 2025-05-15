5de0ff3b8d14fcaf7dbbd198afe9191496122b51
Dorota Bryczkowska <dorotabryczkowska755@gmail.com>
2025-05-16 01:01:12 +0200

    Login template
    Added login template and created connections between login page and logged in users list page.

--- base/views.py
+++ base/views.py
@@ -1,12 +1,10 @@
 "Django view for the todo list application."
-from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
-from django.contrib.auth.views import LoginView, LogoutView
-
+from django.contrib.auth.views import LoginView
 from .models import Task
 
 class CustomLoginView(LoginView):
