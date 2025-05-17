46e3279485dc28a5d3c994446372d4fa4d24d04a
Dorota Bryczkowska <dorotabryczkowska755@gmail.com>
2025-05-17 23:37:42 +0200

    Added Mixin
    Added mixin that restricts users from accessing the list if they're not logged in and redirects them to the login page.

--- base/views.py
+++ base/views.py
@@ -5,6 +5,7 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Task
 
 class CustomLoginView(LoginView):
@@ -16,7 +17,7 @@ class CustomLoginView(LoginView):
     def get_success_url(self):
         return reverse_lazy('tasks')
 
-class TaskList(ListView):
+class TaskList(LoginRequiredMixin, ListView):
     "Django view for displaying a list of tasks."
     model = Task
     context_object_name = 'tasks'
